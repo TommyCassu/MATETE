6f8bf0188d285cbdd39af70157733d9c43346519
RomainChardon <romain.chardon.sio2020@gmail.com>
2021-10-11 14:09:34 +0200

    ajout des points

--- matete-web/src/Controller/MainController.php
+++ matete-web/src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
+use App\Entity\Lieu;
 use App\Repository\AnnonceRepository;
 use App\Repository\LieuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,10 +15,26 @@ class MainController extends AbstractController
     #[Route('/', name: 'main')]
     public function index(AnnonceRepository $annonceRepository, LieuRepository $lieuRepository): Response
     {
-        return $this->render('main/index.html.twig', [
-            'annonces' => $annonceRepository->findAll(),
-            'lieux' => $lieuRepository->findAll(),
+        $lieux = $lieuRepository->findAll();
 
+
+        $listeAnnonces = [];
+        foreach ($lieux as $lieu) {
+            foreach ($lieu->getAnnonce() as $annonce) {
+                $name = $lieu->getNom();
+                $cooX = $lieu->getCooX();
+                $cooY = $lieu->getCooY();
+                
+                $listeAnnonces[] = array(
+                    'name' => $name,
+                    'cooX'=> $cooX,
+                    'cooY' => $cooY
+                );
+            }
+        }
+
+        return $this->render('main/index.html.twig', [
+            'listeAnnonces' => $listeAnnonces,
         ]);
     }
 }

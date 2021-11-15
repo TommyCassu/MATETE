3a88bc68ff032525d713c228e19e049018806a33
RomainChardon <romain.chardon.sio2020@gmail.com>
2021-11-15 13:59:40 +0100

    connexion REUSSI

--- matete-web/src/Security/LoginFormAuthenticator.php
+++ matete-web/src/Security/LoginFormAuthenticator.php
@@ -9,10 +9,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\PasswordUpgradeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
@@ -40,7 +39,7 @@ class LoginFormAuthenticator extends AbstractAuthenticator
             throw new CustomUserMessageAuthenticationException('Username non valide');
         }
 
-        return new Passport( $producteur, 
+        return new Passport( new UserBadge($request->request->get('mail')), 
         new PasswordCredentials($request->request->get('pass')), [
             new CsrfTokenBadge('log_form', $request->request->get('csrf_token'))
         ]);
